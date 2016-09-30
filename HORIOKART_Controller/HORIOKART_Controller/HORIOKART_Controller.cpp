c867044634cbc7c47339123cfe9d9be10199b1c1
ichi-k <k.ichikawa.mh@gmail.com>
2016-09-30 22:58:55 +0900

    うんこ

--- HORIOKART_Controller/HORIOKART_Controller/HORIOKART_Controller.cpp
+++ HORIOKART_Controller/HORIOKART_Controller/HORIOKART_Controller.cpp
@@ -146,7 +146,7 @@ int main(int argc, _TCHAR* argv[])
 		return -1;			//ypspurの初期化
 
 	getArduinoHandle(hCom);		//Arduinoのハンドルを取得する
-
+	
 	
 	
 	YP_set_wheel_vel((1000 * MAX_VEL / 3600) / (TIRE_R / 2), (1000 *MAX_VEL / 3600) / (TIRE_R / 2));
@@ -173,166 +173,162 @@ int main(int argc, _TCHAR* argv[])
 
 		state = (int)arduino_state[0];
 		//printf("%d\n", (int)arduino_state[0]);
-
-		read_states(state, button_state);
-
 		//非常停止ボタンが押されたときの動作
-		//プログラムによる非常停止であり，補助的役割
-		//一度押されたら5秒間他の動作を受け付けない，
-		//押された後フリーズ状態となり，もう一度押されるまで反応しない
-		if (button_state[5] == 1){
-			if (emergency_state){
-				//非常停止状態で押されたときは停止指令を送った上で復帰する
-				Spur_stop();
-				Spur_unfreeze();
-				emergency_state = 0;
-				Sleep(1000);
-				//解除後はボタン誤操作防止のため1秒間停止する
-			}
-
+		if (state == 99){
 			Spur_stop();
-			emergency_state = 1;
-			Sleep(5000);
-
-			
-		}
-
-		//車輪ロックの解除ボタンの動作
-		if (button_state[4] == 1){
-			//printf("free");
-			Spur_free();
-			before_state = 0;
-		}
-
-
-		//条件分岐によりボタンに対する動作を割り振る
-		//同時押しを先に分岐し定義，後に別で押したときの動作を定義
-		//どの条件にも当てはまらない(else)のときはstopさせる
-
-		//front/left
-		//左方向に内側と外側の車輪の速度を1：2に
-		else if ((button_state[0] == 1) && (button_state[2] == 1)){
-			if (before_state != 1){
-				YP_wheel_vel(-ang_vel / 2, ang_vel);
-				//printf("status update\nfront/right\n");
-			}
-			
-			before_state = 1;
-			
-		}
-		//front/right
-		else if((button_state[0] == 1) && (button_state[3] == 1)){
-			if (before_state != 2){
-				YP_wheel_vel(-ang_vel, ang_vel / 2);
-				//printf("status update\nfront/right\n");
-			}
-
-			before_state = 2;
-		}
-
-		//back/left
-		else if ((button_state[1] == 1) && (button_state[2] == 1)){
-			if (before_state != 3){
-				YP_wheel_vel(ang_vel / 2, -ang_vel);
-				//printf("status update\nback/left\n");
-			}
-
-			before_state = 3;
-		}
-
-
-		//back/right
-		else if ((button_state[1] == 1) && (button_state[3] == 1)){
-			if (before_state != 4){
-				YP_wheel_vel(-ang_vel, ang_vel / 2);
-				//printf("status update\nback/right\n");
-			}
-			before_state = 4;
-		}
-
-		//front
-		else if (button_state[0] == 1){
-
-			if (before_state != 5){
-				//YP_wheel_vel(-(MAX_VEL / 3600) / TIRE_R, (MAX_VEL / 3600) / TIRE_R);
-				YP_wheel_vel(-ang_vel, ang_vel);
-				//printf("status update\nfront\n");
-			}
-
-			before_state = 5;
-
-		}
-
-		//back
-		else if (button_state[1] == 1){
-			if (before_state != 7){
-				YP_wheel_vel(ang_vel, -ang_vel);
-				//printf("status update\nback\n");
-			}
-			before_state = 7;
-		}
-
-		//left
-		//左に曲がるように両輪を逆回転
-		else if (button_state[2] == 1){
-			if (before_state != 8){
-				YP_wheel_vel(ang_vel, ang_vel);
-				//printf("status update\nleft\n");
-			}
-			before_state = 8;
-		}
-		//right
-		else if (button_state[3] == 1){
-			if (before_state != 9){
-				YP_wheel_vel(-ang_vel, -ang_vel);
-				//printf("status update\nright\n");
-			}
-
-			before_state = 9;
 		}
 
 		else{
-			if (before_state != 0){
-				//printf("stop");
-				YP_set_wheel_accel(6.28, 6.28);
-				YP_wheel_vel(0.0, 0.0);
-				YP_set_wheel_accel(1.5, 1.5);
-				//Spur_stop();
+			read_states(state, button_state);
+
+
+			if (button_state[5] == 1){
+
+				Spur_stop();
+
 				before_state = 10;
+
 			}
-				
-			//printf("Stop");
-			
-		}
 
-		//printf("\n%d\n", before_state);
-		
-
-		//printf("現在角速度:%lf,%lf  差:%lf\n", vr, vl,vr+vl);
-
-		Sleep(50);
-		standby_count++;
-		
-		YP_get_wheel_vel(&vr, &vl);
-		
-		if (standby_count > 100){
-			if (((vr + vl) / 2) == 0.0){
-				if (before_state == 10){
-					Spur_stop();
-				}
-				else if (before_state == 0){
-					Spur_free();
-				}
+			//車輪ロックの解除ボタンの動作
+			else if (button_state[4] == 1){
+				//printf("free");
+				Spur_free();
+				before_state = 0;
 			}
-			standby_count = 0;
-		}
 
+
+			//条件分岐によりボタンに対する動作を割り振る
+			//同時押しを先に分岐し定義，後に別で押したときの動作を定義
+			//どの条件にも当てはまらない(else)のときはstopさせる
+
+			//front/left
+			//左方向に内側と外側の車輪の速度を1：2に
+			else if ((button_state[0] == 1) && (button_state[2] == 1)){
+				if (before_state != 1){
+					YP_wheel_vel(-ang_vel / 2, ang_vel);
+					//printf("status update\nfront/right\n");
+				}
+
+				before_state = 1;
+
+			}
+			//front/right
+			else if ((button_state[0] == 1) && (button_state[3] == 1)){
+				if (before_state != 2){
+					YP_wheel_vel(-ang_vel, ang_vel / 2);
+					//printf("status update\nfront/right\n");
+				}
+
+				before_state = 2;
+			}
+
+			//back/left
+			else if ((button_state[1] == 1) && (button_state[2] == 1)){
+				if (before_state != 3){
+					YP_wheel_vel(ang_vel / 2, -ang_vel);
+					//printf("status update\nback/left\n");
+				}
+
+				before_state = 3;
+			}
+
+
+			//back/right
+			else if ((button_state[1] == 1) && (button_state[3] == 1)){
+				if (before_state != 4){
+					YP_wheel_vel(-ang_vel, ang_vel / 2);
+					//printf("status update\nback/right\n");
+				}
+				before_state = 4;
+			}
+
+			//front
+			else if (button_state[0] == 1){
+
+				if (before_state != 5){
+					//YP_wheel_vel(-(MAX_VEL / 3600) / TIRE_R, (MAX_VEL / 3600) / TIRE_R);
+					YP_wheel_vel(-ang_vel, ang_vel);
+					//printf("status update\nfront\n");
+				}
+
+				before_state = 5;
+
+			}
+
+			//back
+			else if (button_state[1] == 1){
+				if (before_state != 7){
+					YP_wheel_vel(ang_vel, -ang_vel);
+					//printf("status update\nback\n");
+				}
+				before_state = 7;
+			}
+
+			//left
+			//左に曲がるように両輪を逆回転
+			else if (button_state[2] == 1){
+				if (before_state != 8){
+					YP_wheel_vel(ang_vel, ang_vel);
+					//printf("status update\nleft\n");
+				}
+				before_state = 8;
+			}
+			//right
+			else if (button_state[3] == 1){
+				if (before_state != 9){
+					YP_wheel_vel(-ang_vel, -ang_vel);
+					//printf("status update\nright\n");
+				}
+
+				before_state = 9;
+			}
+
+
+			else{
+				if ((before_state != 0) && (before_state != 10)){
+					//printf("stop");
+					YP_set_wheel_accel(6.28, 6.28);
+					YP_wheel_vel(0.0, 0.0);
+					YP_set_wheel_accel(1.5, 1.5);
+					//Spur_stop();
+					before_state = 10;
+				}
+
+				//printf("Stop");
+
+			}
+
+			//printf("\n%d\n", before_state);
+
+
+			//printf("現在角速度:%lf,%lf  差:%lf\n", vr, vl,vr+vl);
+
+			Sleep(50);
+			standby_count++;
+
+			YP_get_wheel_vel(&vr, &vl);
+
+			if (standby_count > 100){
+				if (((vr + vl) / 2) == 0.0){
+					if (before_state == 10){
+						Spur_stop();
+					}
+					else if (before_state == 0){
+						Spur_free();
+					}
+				}
+				standby_count = 0;
+			}
+
+
+		}
 
 
 
 
 	}
-
+	
 
 	return 0;
 }
--- controler_arduino/controler_arduino.ino
+++ controler_arduino/controler_arduino.ino
@@ -1,10 +1,11 @@
-#define button_front 2
-#define button_back 3
+#define button_front 5
+#define button_back 2
 #define button_left 4
-#define button_right 5
+#define button_right 3
 
 #define button_free 6
-#define button_emerg 7
+#define button_stop 7
+#define emergency_button 9
 
 int front_state = 0;
 int back_state = 0;
@@ -23,65 +24,77 @@ void setup() {
   pinMode(button_right, INPUT_PULLUP);
 
   pinMode(button_free, INPUT_PULLUP);
-  pinMode(button_emerg, INPUT_PULLUP);
+  pinMode(button_stop, INPUT_PULLUP);
+
+  pinMode(emergency_button, INPUT_PULLUP);
 
   Serial.begin(9600);
 
-  
+
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  //各ボタンのステータスを取得する
-  if (digitalRead(button_front) == LOW) {
-    front_state = 1;
+  //非常停止ボタンのみ先に読み取り検知したら即座に指令を送る
+  if (digitalRead(emergency_button) == HIGH) {
+    Serial.write(99);
   }
+
+  //非常停止ボタンが押されていなければコントローラの指令を読む
   else {
-    front_state = 0;
-  }
 
-  if (digitalRead(button_back) == LOW) {
-    back_state = 1;
-  }
-  else {
-    back_state = 0;
-  }
+    //各ボタンのステータスを取得する
+    if (digitalRead(button_front) == LOW) {
+      front_state = 1;
+    }
+    else {
+      front_state = 0;
+    }
 
-  if (digitalRead(button_left) == LOW) {
-    left_state = 1;
-  }
-  else {
-    left_state = 0;
-  }
+    if (digitalRead(button_back) == LOW) {
+      back_state = 1;
+    }
+    else {
+      back_state = 0;
+    }
 
-  if (digitalRead(button_right) == LOW) {
-    right_state = 1;
-  }
-  else {
-    right_state = 0;
-  }
+    if (digitalRead(button_left) == LOW) {
+      left_state = 1;
+    }
+    else {
+      left_state = 0;
+    }
 
-  if (digitalRead(button_free) == LOW) {
-    free_state = 1;
-  }
-  else {
-    free_state = 0;
-  }
+    if (digitalRead(button_right) == LOW) {
+      right_state = 1;
+    }
+    else {
+      right_state = 0;
+    }
 
-  if (digitalRead(button_emerg) == LOW) {
-    emerg_state = 1;
+    if (digitalRead(button_free) == LOW) {
+      free_state = 1;
+    }
+    else {
+      free_state = 0;
+    }
+
+    if (digitalRead(button_stop) == LOW) {
+      emerg_state = 1;
+    }
+    else {
+      emerg_state = 0;
+    }
+
+    
+
+    //2進数から10進数に変換
+    state = 1 * front_state + 2 * back_state + 4 * left_state + 8 * right_state + 16 * free_state + 32 * emerg_state;
+
+    Serial.write(state);
   }
-  else {
-    emerg_state = 0;
-  }
-
-  //2進数から10進数に変換
-  state = 1 * front_state + 2 * back_state + 4 * left_state + 8 * right_state + 16 * free_state + 32 * emerg_state;
-
-  Serial.write(state);
-
   delay(50);
 
 }
